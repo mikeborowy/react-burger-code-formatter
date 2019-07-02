d5d7fa25ee30d7c1edaae0ec7536d0a3f68ad51c
mikeborowy@gmail.com <mikeborowy@gmail.com>
2019-07-03 00:05:46 +0200

    Added Burger

--- src/components/common/burger/Burger.jsx
+++ src/components/common/burger/Burger.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './burger.scss';
-import Ingredient from './ingredient/Ingredient';
+import { Ingredient } from './ingredient/Ingredient';
 import { INGREDIENTS } from '../../../constants/ingredients';
 
 /**
