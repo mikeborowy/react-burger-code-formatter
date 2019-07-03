83a6bb31f3ed27756744004d866dea0235a7b775
Mike Borowy <mike@memcare.com>
2019-07-03 11:55:27 +0200

    Updated paths

--- src/config/store/config.prod.js
+++ src/config/store/config.prod.js
@@ -1,9 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
 import reduxPromise from 'redux-promise';
-import { async } from '../../middlewares/async';
-import { rootReducers } from '../../reducers';
-import { stateValidator } from '../../middlewares/stateValidator';
+import { async } from '../../store/middlewares/async';
+import { rootReducers } from './index';
+import { stateValidator } from '../../store/middlewares/stateValidator';
 
-export const config = () =>
-  createStore(rootReducers, applyMiddleware(reduxThunk, reduxPromise, async));
+export const config = () => {
+  return createStore(rootReducers, applyMiddleware(reduxThunk, reduxPromise, async));
+};
--- src/store/middlewares/stateValidator.js
+++ src/store/middlewares/stateValidator.js
@@ -1,10 +1,14 @@
 import tv4 from 'tv4';
 import stateSchema from '../../constants/stateSchema';
 
-export const stateValidator = ({ dispatch, getState }) => (next) => (action) => {
-  next(action);
-  if (!tv4.validate(getState(), stateSchema)) {
-    /* eslint-disable-next-line */
-    console.error('Invalid state schema detected');
-  }
+export const stateValidator = ({ dispatch, getState }) => {
+  return (next) => {
+    return (action) => {
+      next(action);
+      if (!tv4.validate(getState(), stateSchema)) {
+        /* eslint-disable-next-line */
+        console.error('Invalid state schema detected');
+      }
+    };
+  };
 };
